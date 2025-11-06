7eab20a202297c67e9578a5b36507abf14f6dbd2
reemzakaria1q2w3e4r-debug <reemzakaria1q2w3e4r@gmail.com>
2025-11-06 18:28:02 +0200

    Add base path to Vite configuration
    Set the base path for the application.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/i-love-my-loly/', // <--- هنا أضفنا base
       server: {
         port: 3000,
         host: '0.0.0.0',
